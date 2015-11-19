47360407e885737a4db7137a302f26df37022b4d
RoccoRiccardi <Rocco.Riccardi.42@gmail.com>
2015-11-19 16:20:11 -0500

    added some stuff to player index view

--- BaseballLeague/BaseballLeague.MVC/Controllers/PlayerController.cs
+++ BaseballLeague/BaseballLeague.MVC/Controllers/PlayerController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using BaseballLeague.BLL;
+using BaseballLeague.MVC.Models;
 
 namespace BaseballLeague.MVC.Controllers
 {
@@ -11,7 +13,13 @@ namespace BaseballLeague.MVC.Controllers
         // GET: Player
         public ActionResult Index()
         {
-            return View();
+            var ops = new BaseballBLL();
+            var players = ops.GetNonFreeAgentPlayers();
+            var teams = ops.GetTeams();
+            
+            var vm = new PlayersVM(players, teams);
+            
+            return View(vm);
         }
     }
 }
