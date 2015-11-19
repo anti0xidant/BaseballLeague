e29877c4ecc78dbc3d37ac2a6779dc49e38a0130
Andrew Tang <atang.business@gmail.com>
2015-11-19 16:10:30 -0500

    added comments to update class

--- BaseballLeague/BaseballLeague.Data/Repo/Update.cs
+++ BaseballLeague/BaseballLeague.Data/Repo/Update.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
@@ -10,7 +11,6 @@ namespace BaseballLeague.Data
 {
     public class Update
     {
-
         // Releases player by changing TeamID to 1 for 'Free Agent'
         public void ReleasePlayer(int PlayerID)
         {
@@ -21,10 +21,35 @@ namespace BaseballLeague.Data
                 try
                 {
                     p.Add("PlayerID", PlayerID);
+
+                    cn.Execute("ReleasePlayer", p, commandType: CommandType.StoredProcedure);
+                }
+                finally
+                {
+                    cn.Close();
                 }
             }
         }
         
         // Swaps player's team by changing TeamID
+        public void TradePlayer(int PlayerID, int TeamID)
+        {
+            using (SqlConnection cn = new SqlConnection())
+            {
+                var p = new DynamicParameters();
+
+                try
+                {
+                    p.Add("PlayerID", PlayerID);
+                    p.Add("TeamID", TeamID);
+
+                    cn.Execute("TradePlayer", p, commandType: CommandType.StoredProcedure);
+                }
+                finally
+                {
+                    cn.Close();
+                }
+            }
+        }
     }
 }
