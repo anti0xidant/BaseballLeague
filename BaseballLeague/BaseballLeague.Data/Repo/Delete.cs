f8f23e7868b74f79b823c0135e00333fda034918
Andrew Tang <atang.business@gmail.com>
2015-11-19 16:18:20 -0500

    added DeletePlayer to data and BLL layers

--- BaseballLeague/BaseballLeague.Data/Repo/Delete.cs
+++ BaseballLeague/BaseballLeague.Data/Repo/Delete.cs
@@ -1,12 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BaseballLeague.Data.Config;
+using Dapper;
 
 namespace BaseballLeague.Data
 {
     public class Delete
     {
+        // Hard deletes a player from Player Table
+        public void DeletePlayer(int PlayerID)
+        {
+            using (SqlConnection cn = new SqlConnection(Settings.ConnectionString))
+            {
+                var p = new DynamicParameters();
+
+                try
+                {
+                    p.Add("PlayerID", PlayerID);
+
+                    cn.Execute("DeletePlayer", p, commandType: CommandType.StoredProcedure);
+                }
+                finally
+                {
+                    cn.Close();
+                }
+            }
+        }
     }
 }
